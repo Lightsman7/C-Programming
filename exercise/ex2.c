d9158c92f885a4a1119a1c90837b16c85813364c
Lightsman <kyawthihasoe499@gmail.com>
2024-03-08 13:44:34 +0700

    create c program
    Date: 8-03-2024

---
+++ exercise/ex2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    const int SIDE = 10;
+    int area;
+    area = SIDE*SIDE;
+    printf("The area of the square with side: %d is: %d sq.units", SIDE, area); 
+}
